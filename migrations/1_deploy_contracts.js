8e930d5023696398d624364a00c420f218d63208
falermana <152500965+falermana@users.noreply.github.com>
2024-05-22 23:16:41 +0800

    Update 1_deploy_contracts.js

--- migrations/1_deploy_contracts.js
+++ migrations/1_deploy_contracts.js
@@ -1,5 +1,7 @@
 const TeaStaking = artifacts.require("TeaStaking");
 
 module.exports = function(deployer) {
-  deployer.deploy(TeaStaking, "<TEA_TOKEN_ADDRESS>");
+  const teaTokenAddress = "<TEA_TOKEN_ADDRESS>"; 
+  const rewardAddress = "<REWARD_ADDRESS>"; 
+  deployer.deploy(TeaStaking, teaTokenAddress, rewardAddress);
 };
