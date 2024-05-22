761ef04d0c29ce08072025eb6d443f6856ad2bfd
falermana <152500965+falermana@users.noreply.github.com>
2024-05-22 23:39:26 +0800

    Create adminService.js

---
+++ src/services/adminService.js
@@ -0,0 +1,11 @@
+class AdminService {
+  constructor(adminInstance) {
+    this.adminInstance = adminInstance;
+  }
+
+  async changeAdminAddress(newAdmin) {
+    // 实现更改管理员地址逻辑
+  }
+}
+
+module.exports = AdminService;
