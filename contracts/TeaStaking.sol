b4bb9bb58232f3b7aa9a8081e198bc8bed7fdbbd
falermana <152500965+falermana@users.noreply.github.com>
2024-05-22 23:23:02 +0800

    Update TeaStaking.sol

--- contracts/TeaStaking.sol
+++ contracts/TeaStaking.sol
@@ -8,22 +8,32 @@ contract TeaStaking {
     mapping(address => uint256) public stakedBalance;
     mapping(address => uint256) public lastUpdateTime;
     mapping(address => uint256) public rewards;
-    mapping(address => address) public rewardAddresses; // 存储用户选择的收益类型和地址
+    mapping(address => address) public rewardAddresses;
+    mapping(address => uint256) public stakePeriods; // 新增：存储用户的质押期限
+    address public admin;
 
     event Staked(address indexed user, uint256 amount);
     event Withdrawn(address indexed user, uint256 amount);
     event RewardPaid(address indexed user, uint256 reward);
+    event RewardTypeSet(address indexed user, string rewardType); // 新增：设置收益类型事件
 
     constructor(address _teaToken) {
         teaToken = IERC20(_teaToken);
+        admin = msg.sender; // 合约部署者作为管理员
     }
 
-    function stake(uint256 amount, address rewardAddress) external {
+    modifier onlyAdmin() {
+        require(msg.sender == admin, "Only admin can call this function");
+        _;
+    }
+
+    function stake(uint256 amount, uint256 period, address rewardAddress) external {
         updateReward(msg.sender);
         require(amount > 0, "Cannot stake 0");
         teaToken.transferFrom(msg.sender, address(this), amount);
         stakedBalance[msg.sender] += amount;
-        rewardAddresses[msg.sender] = rewardAddress; // 设置用户选择的收益类型和地址
+        rewardAddresses[msg.sender] = rewardAddress;
+        stakePeriods[msg.sender] = period; // 设置用户的质押期限
         lastUpdateTime[msg.sender] = block.timestamp;
         emit Staked(msg.sender, amount);
     }
@@ -41,11 +51,17 @@ contract TeaStaking {
         uint256 reward = rewards[msg.sender];
         if (reward > 0) {
             rewards[msg.sender] = 0;
-            address rewardAddress = rewardAddresses[msg.sender]; // 获取用户选择的收益类型地址
+            address rewardAddress = rewardAddresses[msg.sender];
             require(rewardAddress != address(0), "Reward address not set");
-            IERC20(rewardAddress).transfer(msg.sender, reward); // 转账相应的收益
+            IERC20(rewardAddress).transfer(msg.sender, reward);
             emit RewardPaid(msg.sender, reward);
         }
+    }
+
+    function setRewardType(address user, string memory rewardType) external onlyAdmin {
+        // 设置用户的收益类型
+        // 可以根据需要进一步扩展，比如检查合法性等
+        emit RewardTypeSet(user, rewardType);
     }
 
     function updateReward(address account) internal {
