d227ccaacc018b1418c85a2f39c217f3cb090674
falermana <152500965+falermana@users.noreply.github.com>
2024-05-22 23:17:59 +0800

    Update stake.js

--- scripts/stake.js
+++ scripts/stake.js
@@ -1,14 +1,14 @@
 const Web3 = require('web3');
-const web3 = new Web3('http://localhost:8545'); // 你的以太坊节点地址
+const web3 = new Web3('http://localhost:8545'); 
 
 const TeaStakingContract = require('./build/contracts/TeaStaking.json');
-const contractAddress = '<CONTRACT_ADDRESS>'; // 合约部署后的地址
+const contractAddress = '<CONTRACT_ADDRESS>'; 
 const teaStakingInstance = new web3.eth.Contract(TeaStakingContract.abi, contractAddress);
 
 const main = async () => {
   const accounts = await web3.eth.getAccounts();
-  const amountToStake = web3.utils.toWei('100', 'ether'); // 要质押的代币数量
-  const rewardAddress = '<REWARD_ADDRESS>'; // 用户选择的收益地址
+  const amountToStake = web3.utils.toWei('100', 'ether'); 
+  const rewardAddress = '<REWARD_ADDRESS>'; 
 
   // 调用质押函数，并传入用户选择的收益地址
   await teaStakingInstance.methods.stake(amountToStake, rewardAddress).send({ from: accounts[0] });
