1cebe70063b9cc110db2d4ff47a6d2f866a3a672
falermana <152500965+falermana@users.noreply.github.com>
2024-05-22 23:40:05 +0800

    Update rewardService.js

--- src/services/rewardService.js
+++ src/services/rewardService.js
@@ -1,1 +1,12 @@
 
+class RewardService {
+  constructor(teaStakingInstance) {
+    this.teaStakingInstance = teaStakingInstance;
+  }
+
+  async distributeRewards() {
+    // 实现奖励分发逻辑
+  }
+}
+
+module.exports = RewardService;
